760ede8eeb6cb659eae3d7598745bff8a423ac22
Patryk Jaworski <patryk.jaworski003@gmail.com>
2022-11-10 11:39:40 +0100

    Add ListElement styles

--- src/components/ListElement/ListElement.jsx
+++ src/components/ListElement/ListElement.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import s from './ListElement.module.css';
+import React from "react";
+import s from "./ListElement.module.css";
 
-const ListElement = () => {
-  return (
-    <div>ListElement
-     <div>
-      count
-      ava
-      track
-     </div>
-     <span>album</span>
-     <span>duration</span>
-     <button>more</button>
-    </div>
-  )
-}
+const ListElement = ({ author, title, ind, album, duration }) => {
+ return (
+  <div class={s.elem}>
+   <div class={s.main}>
+    <span>{ind}</span>
+    <span>
+     {author} - {title}
+    </span>
+   </div>
+   <span class={s.album}>{album}</span>
+   <span class={s.dur}>{duration}</span>
+   <button class={s.cnt_btn}>more</button>
+  </div>
+ );
+};
+
+ListElement.defaultProps = {
+ author: "Untitled Author",
+ title: "Untitled Title",
+ ind: "-",
+ album: "Not set",
+ duration: "-",
+};
 
 export default ListElement;
