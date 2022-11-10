d47cbf618d94d8291a51e1fc1fc60f03494049f0
Patryk Jaworski <patryk.jaworski003@gmail.com>
2022-11-10 14:26:52 +0100

    Add HTMLAudioElement, set pause play

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Header from '../Header/Header';
-import ListElement from '../ListElement/ListElement';
-import PlayBar from '../PlayBar/PlayBar';
+import React, { useEffect, useState } from "react";
+import Header from "../Header/Header";
+import ListElement from "../ListElement/ListElement";
+import PlayBar from "../PlayBar/PlayBar";
 
 const Main = () => {
-  return (
-    <div>
-     <Header/>
-     <ListElement/>
-     <PlayBar/>
-    </div>
-  )
-}
+ const [trackUrl, setTrack] = useState(null);
+ const audioApi = new Audio(trackUrl);
+
+ useEffect(() => {
+  setTrack("/Trentemøller - Moan.mp3");
+ }, []);
+ return (
+  <div>
+   <Header />
+   <ListElement />
+   <PlayBar audioApi={audioApi} />
+  </div>
+ );
+};
 
 export default Main;
--- src/components/PlayBar/PlayBar.jsx
+++ src/components/PlayBar/PlayBar.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import s from "./s.module.css";
 
-const PlayBar = () => {
+const PlayBar = ({ audioApi }) => {
+ const [volume, setVolume] = useState(0.4);
+ const [isPlaying, setPlay] = useState(false);
+
+ const togglePlay = () => {
+  if (audioApi.paused) {
+   audioApi.play();
+  } else {
+   audioApi.pause();
+  }
+ };
+
+ useEffect(() => {
+  const setPlaying = (bool) => {
+   if (isPlaying !== bool) {
+    setPlay(bool);
+   }
+  };
+  let a = audioApi.addEventListener("play", () => setPlaying(true));
+  let b = audioApi.addEventListener("pause", () => setPlaying(false));
+
+  return () => {
+   audioApi.removeEventListener("play", a);
+   audioApi.removeEventListener("pause", b);
+  };
+ }, [audioApi, isPlaying]);
+
  return (
   <>
    <div className={s.playbar}>
     <div className={s.controls}>
      <button>prev</button>
-     <button>play</button>
+     <button onClick={togglePlay}>{isPlaying ? "pause" : "play"}</button>
      <button>next</button>
     </div>
    </div>
